7bf5e6f82b15fae3831d72f5e7a152194f4356cc
Matthew Shield <matthewshield@hotmail.co.uk>
2017-08-08 15:37:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/user/wordcounter/Counter.java
@@ -0,0 +1,9 @@
+package com.example.user.wordcounter;
+
+/**
+ * Created by user on 08/08/2017.
+ */
+
+public class Counter {
+
+}
---
+++ app/src/test/java/com/example/user/wordcounter/CounterTest.java
@@ -0,0 +1,11 @@
+package com.example.user.wordcounter;
+
+/**
+ * Created by user on 08/08/2017.
+ */
+
+public class CounterTest {
+
+
+
+}
